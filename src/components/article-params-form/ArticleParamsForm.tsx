107736317e43c55d0fbbac761090c8d67cc3fab6
Alex-Mao0804 <160096381+Alex-Mao0804@users.noreply.github.com>
2024-09-28 07:41:51 +0300

    feat: arrow run

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,25 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
+import { useState } from 'react';
+
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpened, setIsOpened] = useState(false);
+	// const isOpened = false;
+	const className = clsx(styles.container, {
+		[styles.container_open]: isOpened,
+	});
+
+	const handleClick = () => {
+		setIsOpened((isOpened) => !isOpened);
+	};
 	return (
 		<>
-			<ArrowButton isOpen={true} onClick={() => {}} />
-			<aside className={clsx(styles.container, styles.container_open)}>
+			<ArrowButton isOpen={isOpened} onClick={handleClick} />
+			<aside className={className}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
